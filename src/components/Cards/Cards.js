36630be2efef160bd7d3899dfecfb9a7f5df08a7
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-14 09:04:22 +0100

    Add table of objects - cards; import to main content of app

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,48 @@
+const cards = [
+    {
+        answer: false,
+        src:"images/bear.png",
+        name: "Bear"
+    },
+    {
+        answer: false,
+        src:"images/cheetah.png",
+        name: "Cheetah"
+    },
+    {
+        answer: false,
+        src:"images/toucan.png",
+        name: "Toucan"
+    },
+    {
+        answer: false,
+        src:"images/elephant.png",
+        name: "Elephant"
+    },
+    {
+        answer: false,
+        src:"images/giraffe.png",
+        name: "Giraffe"
+    },
+    {
+        answer: false,
+        src:"images/zebra.png",
+        name: "Zebra"
+    },
+    {
+        answer: false,
+        src:"images/panda.png",
+        name: "Panda"
+    },
+    {
+        answer: false,
+        src:"images/fawn.png",
+        name: "Fawn"
+    }
+]
+
+export default function Cards() {
+    return (
+        <div>Tutaj będą karty</div>
+    )
+}
--- src/components/Main_Content/MainContent.js
+++ src/components/Main_Content/MainContent.js
@@ -1,10 +1,12 @@
 import "./_mainApp.scss";
+import Cards from "../Cards/Cards";
 
 export default function MainContent() {
     return (
         <div>
             <h1>Welcome!</h1>
             <p>Have fun!</p>
+            <Cards />
         </div>
     )
 }
