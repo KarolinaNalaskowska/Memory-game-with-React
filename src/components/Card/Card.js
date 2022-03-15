c96d74269fc465fc0968355e3580b3d4767eb30c
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-15 12:48:35 +0100

    Add classes to divs; solved problems with flipping cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,20 @@
 import React from "react";
+import classnames from "classnames";
 import "./_card.scss";
 
-export default function Card ({ onClick, card, index, isInactive, flipped, clickable}) {
-
+export default function Card ({ onClick, card, index, inactive, flipped, clickable}) {
     const handleClick = () => {
-        clickable && onClick(index);
-        console.log(index);
+        !flipped && !clickable && onClick(index);
     }
 
+    const classes = classnames(
+        "card", {
+        "flipped": flipped,
+        "inactive": inactive
+    });
+
     return (
-        <div className="card" onClick={handleClick} >
+        <div className={classes} onClick={handleClick} >
             <div className="card-image card-image-revers">
                 <img src="images/cover.png" alt="Revers of the images" />
             </div>
