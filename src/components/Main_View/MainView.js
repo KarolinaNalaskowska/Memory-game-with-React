60004111ffa1d8e34ec8914d0a7b973510569e6d
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-15 15:08:09 +0100

    Add classes and styles

--- src/components/Main_View/MainView.js
+++ src/components/Main_View/MainView.js
@@ -1,9 +1,17 @@
 import "./_mainview.scss";
+import "../../components/general/_general.scss";
 
 export default function MainView() {
     return (
-        <div>
-            <a href="/memoapp">Start Game</a>
+        <div className="background">
+            <div className="mainview mainview__container container">
+                <div className="mainview__content">
+                    <h1 className="title">Welcome to my memory card game!</h1>
+                    <div className="mainview__button">
+                        <a href="/memoapp" className="btn btn__first">Start Game</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
