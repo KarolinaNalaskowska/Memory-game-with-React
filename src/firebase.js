99d88a62edfca65c123c49951d2024c19188aee4
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-16 22:03:25 +0100

    Add and configure firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,23 @@
-import firebase from "firebase"
-const config = {
-    apiKey: "…",
-    authDomain: "…",
-    databaseURL: "…",
-    projectId: "…",
-    storageBucket: "…",
-    messagingSenderId: "…"
-};
-firebase.initializeApp(config);
-const db = firebase.firestore();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
 
-export {db}
-export default firebase;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCWsJK9QEmNtPd-0iyla9FAevcuJVH9vas",
+    authDomain: "memory-game-kn.firebaseapp.com",
+    projectId: "memory-game-kn",
+    storageBucket: "memory-game-kn.appspot.com",
+    messagingSenderId: "345545887263",
+    appId: "1:345545887263:web:1308947b47c52b34044f2a",
+    measurementId: "G-FGXKRDYC7P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
