c2237538c25136b42dd664a58c64df05fb5989de
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-17 16:25:11 +0100

    Add classes, add styles to form files

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import "./_form.scss";
 
-export default function Form ({ nickname, userAge }) {
-
+export default function Form ({ nick, setNick, addNewUser }) {
     return (
-        <div className="form">
-            <p className="form__text">Leave you nickname</p>
-            <form>
-                <input type="text" placeholder="Your nickname" />
-                <button type="submit">Send</button>
+        <div className="form__container">
+            <p className="form-text">Leave your nickname here:</p>
+            <form className="form">
+                <input
+                    className="form__input"
+                    maxLength="20"
+                    value={nick}
+                    type="text"
+                    placeholder="Your nickname"
+                    onChange={setNick}
+                />
+                <div className="form__button" onClick={addNewUser}>Send</div>
             </form>
         </div>
     )
