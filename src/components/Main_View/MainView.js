d3b7729407cedf4490becaf2659b4b5b8e33da55
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-17 16:15:58 +0100

    Add classes, add styles; solved problems; add props

--- src/components/Main_View/MainView.js
+++ src/components/Main_View/MainView.js
@@ -3,20 +3,19 @@ import "./_mainview.scss";
 import "../../components/general/_general.scss";
 import Form from "../Form/Form";
 
-export default function MainView({ nickname, userAge }) {
-
+export default function MainView({ nick, setNick, addNewUser }) {
     return (
         <div className="background">
             <div className="mainview mainview__container container">
                 <div className="mainview__content">
                     <h1 className="mainview__title">Welcome to the memory game!</h1>
                     <a href="/rules" className="question">Do you know the rules?</a>
-                    <Form nickname={nickname}/>
+                    <Form nick={nick} setNick={setNick} addNewUser={addNewUser} />
                     <div className="mainview__button">
                         <a href="/memoapp" className="btn btn__first">Start Game</a>
-                        <a href="/bestscores" className="btn btn__second">Best Scores</a>
+                        <a href="/bestscores" className="btn">Best Scores</a>
                     </div>
-                </div>
+                </div   
             </div>
         </div>
     )
