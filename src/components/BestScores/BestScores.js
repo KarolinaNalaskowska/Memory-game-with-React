109d80450fb6011afc7dd63776a48e5a29aa337b
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-17 16:54:37 +0100

    Add classes, add styles

--- src/components/BestScores/BestScores.js
+++ src/components/BestScores/BestScores.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import "./_bestscores.scss";
 
 export default function BestScores() {
     return (
-        <div>
-            <div>
-                <a href="/">Main Page</a>
-                <a href="/memoapp">Game</a>
+        <div className="table__background">
+            <div className="container table__container">
+            <div className="table__buttons">
+                <a href="/" className="btn btn__table">Main Page</a>
+                <a href="/memoapp" className="btn btn__table">Game</a>
             </div>
-        <table>
-            <thead>
-            <tr>
-                <th>Names</th>
-                <th>Scores</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>Bob</td>
-                <td>10</td>
-            </tr>
-            </tbody>
-        </table>
+            <div className="tableCover">
+                <div>
+                <table className="table">
+                    <thead>
+                    <tr className="table__head">
+                        <th className="table__th">Names</th>
+                        <th className="table__th">Scores</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr className="table__body">
+                        <td className="table__td">Bob</td>
+                        <td className="table__td">10</td>
+                    </tr>
+                    <tr className="table__body">
+                        <td className="table__td">Julia</td>
+                        <td className="table__td">14</td>
+                    </tr>
+                    <tr className="table__body">
+                        <td className="table__td">Adam</td>
+                        <td className="table__td">17</td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
