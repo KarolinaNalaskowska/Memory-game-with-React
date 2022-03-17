3c64fb170b7217aa1b31cf902b0ef27a03bc7fa9
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-17 16:09:27 +0100

    Add functions - sending user name to external database; cleaned code; import props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./main.scss";
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import db from "./firebase";
-import { useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore/lite";
-
+import { useState, useEffect } from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import MainView from "./components/Main_View/MainView";
 import MainContent from "./components/Main_Content/MainContent";
 import BestScores from "./components/BestScores/BestScores";
@@ -14,27 +12,36 @@ async function getUsers(db) {
     const usersCol = collection(db, "users");
     const userSnapshot = await getDocs(usersCol);
     const userList = userSnapshot.docs.map((doc) => doc.data());
+    const users = userList.map((el) => el.nickname);
     console.log(userList);
+    console.log(users);
 }
 
-function App() {
+export default function App() {
+    const [inputValue, setInputValue] = useState("");
+    const ordersCollection = collection(db, 'users');
     useEffect(() => {
-        console.log(db);
         getUsers(db);
     });
+    function addNewUser() {
+        const newDoc = addDoc(ordersCollection, {
+            nickname: inputValue
+        });
+    }
+    const setNick = (e) => setInputValue(e.target.value);
 
-  return (
-      <div>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="/" element={<MainView nickname={db}/>} />
-                  <Route path="/memoapp" element={<MainContent />} />
-                  <Route path="/bestscores" element={<BestScores />} />
-                  <Route path="/rules" element={<Rules />} />
-              </Routes>
-          </BrowserRouter>
-      </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={
+                        <MainView nick={inputValue} setNick={setNick} addNewUser={addNewUser} />
+                    } />
+                    <Route path="/memoapp" element={<MainContent />} />
+                    <Route path="/bestscores" element={<BestScores />} />
+                    <Route path="/rules" element={<Rules />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
-
-export default App;
