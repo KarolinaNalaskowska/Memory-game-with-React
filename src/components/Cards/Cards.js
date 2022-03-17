69f40ecd09f1cb2c5030fc140af52d9c6b149318
Karolina <nalaskowska.karolina3@gmail.com>
2022-03-17 16:38:16 +0100

    import general stylesheet

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useEffect, useState} from "react";
 import "./_cards.scss";
+import "../../components/general/_general.scss";
 import Card from "../Card/Card";
 
 const animalCards = [
